ecbef221a469249f74edac315d6407aa6deb4f55
batabalazs96 <43841279+batabalazs96@users.noreply.github.com>
2021-09-22 15:22:38 +0200

    create the basics popup

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import Map from 'ol/Map';
+import Overlay from 'ol/Overlay';
 import View from 'ol/View';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
 import {fromLonLat} from 'ol/proj';
+import {toStringHDMS} from 'ol/coordinate';
+import {toLonLat} from 'ol/proj';
 
 @Component({
   selector: 'app-map',
@@ -11,14 +14,33 @@ import {fromLonLat} from 'ol/proj';
   styleUrls: ['./map.component.css']
 })
 export class MapComponent implements OnInit {
-  map!: Map;
+  map: Map;
   
   ngOnInit(): void {
+    const container = document.getElementById('popup');
+    const content = document.getElementById('popup-content');
+    const closer = document.getElementById('popup-closer');
+
+    const overlay = new Overlay({
+      element : container,
+      autoPan: true,
+      autoPanAnimation: {
+        duration: 250,
+      },
+    });
+
+    closer.onclick = function () {
+      overlay.setPosition(undefined);
+      closer.blur();
+      return false;
+    };
+
     this.map = new Map({
       view: new View({
         center: fromLonLat([19.140789,47.075343]),
         zoom: 8,
       }),
+      overlays : [overlay],
       layers: [
         new TileLayer({
           source: new OSM(),
@@ -26,5 +48,14 @@ export class MapComponent implements OnInit {
       ],
       target: 'ol-map'
     });
+    this.map.on('singleclick', function (evt) {
+      const coordinate = evt.coordinate;
+      const hdms = toStringHDMS(toLonLat(coordinate));
+    
+      content.innerHTML = '<p>You clicked here:</p><code>' + hdms + '</code>';
+      overlay.setPosition(coordinate);
+      
+    });
     }
+    
 }
